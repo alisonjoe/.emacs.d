dbf657d1c9cfebee674c7be83e040f4883182c82
alisonjoe <Mr.Lai.who@gmail.com>
2016-06-13 02:59:32 +0800

    add fulltime modify keymap \c+x to \c+c

--- lisp/editing-utils/init-gettime.el
+++ lisp/editing-utils/init-gettime.el
@@ -4,17 +4,22 @@
 (defun insert-current-date ()
     "Insert the current date"
     (interactive "*")
-    ;(insert (format-time-string "%Y-%m-%d %H:%M:%S" (current-time))))
     (insert (format-time-string "%Y-%m-%d" (current-time))))
-    (global-set-key "\C-xd" 'insert-current-date)
+    (global-set-key "\C-cd" 'insert-current-date)
 
 
 ;;insert-current-time
 (defun insert-current-time ()
     "Insert the current time"
     (interactive "*")
-    ;(insert (format-time-string "%Y/%m/%d %H:%M:%S" (current-time))))
     (insert (format-time-string "%H:%M:%S" (current-time))))
-    (global-set-key "\C-xt" 'insert-current-time)
+    (global-set-key "\C-ct" 'insert-current-time)
+
+;;insert-current-fulltime
+(defun insert-current-fulltime ()
+  "Insert the current fulltime"
+  (interactive "*")
+  (insert (format-time-string "%Y/%m/%d %H:%M:%S" (current-time))))
+  (global-set-key "\C-ci" 'insert-current-fulltime)
 
 (provide 'init-gettime)
