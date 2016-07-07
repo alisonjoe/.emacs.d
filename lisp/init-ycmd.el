6d317ddd41fa0c30d24003d12f2ff600e85b71c4
alisonjoe <hongshu.lai@quncaotech.com>
2016-07-08 02:59:01 +0800

    add snippets

--- lisp/init-ycmd.el
+++ lisp/init-ycmd.el
@@ -2,11 +2,11 @@
 
 ;; ycmd
 (add-hook 'c++-mode-hook 'ycmd-mode)
-(set-variable 'ycmd-server-command '("python" "/home/alisonjoe/Tools/ycmd/ycmd")) ;; ycmd安装路径
+(set-variable 'ycmd-server-command '("python" "/home/alisonjoe/Tools/ycmd/ycmd/")) ;; ycmd安装路径
 (set-variable 'ycmd-global-config "~/.emacs.d/ycm/.ycm_extra_conf.py")
 (set-variable 'ycmd-extra-conf-whitelist
             '("~/.emacs.d/ycm/*")
-            '("/home/alisonjoe/WorkSpace/bigfan_broka/*"))
+            '("~/WorkSpace/bigfan_broka/*"))
 
 (defun ycmd-setup-completion-at-point-function ()
   "Setup `completion-at-point-functions' for `ycmd-mode'."
