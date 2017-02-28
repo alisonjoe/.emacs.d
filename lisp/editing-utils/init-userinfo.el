9bca4b13c79a40e3daccd4a59a12f2777c5f747d
Mr.Lai.who <laihongshu@JD.248>
2017-02-28 10:20:39 +0800

    恢复userinfo.el

---
+++ lisp/editing-utils/init-userinfo.el
@@ -0,0 +1,12 @@
+;; init-userinfo.el
+;; Thu Apr 21 13:12:39 CST 2016
+
+;; set user name
+(setq user-full-name "xxx")
+
+;; set user email
+(setq enterprise-mail "xxx@xx.com")
+(setq personal-mail "xxx@xx.com")
+
+
+(provide 'init-userinfo)
