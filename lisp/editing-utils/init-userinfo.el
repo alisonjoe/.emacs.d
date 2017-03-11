d36f3f4dd3911f582c6ca451aefbcb927b2ac83c
Mr.Lai.who <laihongshu@JD.248>
2017-03-11 15:23:35 +0800

    个人邮箱el

---
+++ lisp/editing-utils/init-userinfo.el
@@ -0,0 +1,12 @@
+;; init-userinfo.el
+;; Thu Apr 21 13:12:39 CST 2016
+
+;; set user name
+(setq user-full-name "Mr.Lai")
+
+;; set user email
+(setq enterprise-mail "alisonjoe@163.com")
+(setq personal-mail "Mr.Lai.who@gmail.com")
+
+
+(provide 'init-userinfo)
