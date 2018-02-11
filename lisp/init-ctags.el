487d3bb58aab460fb1efedcd16ee8f4bf3cfbb7b
alison <alison@alisondeMacBook-Pro.local>
2018-02-12 02:29:28 +0800

    delete ctags

--- lisp/init-ctags.el
+++
@@ -1,8 +0,0 @@
-(require-package 'ctags)
-
-(setq tags-revert-without-query t)
-(global-set-key (kbd "<f7>") 'ctags-create-or-update-tags-table)
-
-(global-set-key (kbd "M-.")  'ctags-search)
-
-(provide 'init-ctags)
